e6bbe3d17eed4628fd7e04196c8c48445de05a86
edwin calvillo <calvillo.edwin@gmail.com>
2020-04-14 22:20:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.include? do |valid_calls[i]|
+  planeteer_calls.include? do |valid_calls, element, i|
     element = valid_calls[i]
   end
 end
